90c4ffdf19ceb305030beb01dd90bdd11783c21b
Ryutaro Tsuji <rt.kametaro@gmail.com>
2023-11-11 17:01:12 +0900

    index 修正

--- js/main.js
+++ js/main.js
@@ -3,11 +3,8 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
-    //document.getElementById("navbar").classList.remove('dark');
-    document.getElementById("myBtn")
   } else {
     document.getElementById("navbar").style.top = "-50px";
-    document.getElementById("myBtn")
   }
 }
 
