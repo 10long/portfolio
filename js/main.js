c3d1186aed15cad2a379bcebb720d10925fea989
Ryutaro Tsuji <rt.kametaro@gmail.com>
2023-11-11 17:13:34 +0900

    Revert "style delete"
    This reverts commit 37f5bfa19749fc2716bb0bd6f86b54a8778ed0e9.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
+    document.getElementById("myBtn").style.display = "block";
   } else {
     document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("myBtn").style.display = "none";
   }
 }
 
